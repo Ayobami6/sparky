1ea882360e998bae3d0d34b35908517ddadc5cbd
Ayobami6 <ayobamidele006@gmail.com>
2024-05-06 12:43:29 +0100

    enable cors and cookie parser

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './app.module';
 import * as process from 'process';
 import { Logger } from '@nestjs/common';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
   logger.log(`Application is running on:  ${await app.getUrl()}`);
