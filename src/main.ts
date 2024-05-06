9f1176fbf80355926af5fb45bd81d8f3b93319b8
Ayobami6 <ayobamidele006@gmail.com>
2024-05-06 11:46:15 +0100

    Update setup complete

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
+import { Logger } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+  logger.log(`Application is running on:  ${await app.getUrl()}`);
 }
 bootstrap();
