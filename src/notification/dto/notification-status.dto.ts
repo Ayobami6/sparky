12072d7cefc7709d23f55f0e65169ac6724d030c
Ayobami Alaran A. <ayobamidele006@gmail.com>
2024-09-11 15:50:13 +0100

    update notification swagger docs schema

--- src/notification/dto/notification-status.dto.ts
+++ src/notification/dto/notification-status.dto.ts
@@ -1,8 +1,16 @@
 import { IsEnum, IsNotEmpty } from 'class-validator';
 import { Status } from '../notification.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class NotificationStatusDto {
   @IsNotEmpty()
   @IsEnum(Status)
+  @ApiProperty({
+    type: 'string',
+    enum: Status,
+    enumName: 'NotificationStatus',
+    example: 'UNREAD',
+    description: 'The status of the notification',
+  })
   status: Status;
 }
--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -2,11 +2,20 @@ import { Controller, Get, Param, Put, UseGuards } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { AdminAuthGuard } from 'src/auth/jwt-admin-authguard';
 import { Message } from 'src/user/types';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiInternalServerErrorResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { NotificationEntity } from './notification.entity';
 
 @UseGuards(AdminAuthGuard)
 @Controller('notifications')
 @ApiTags('notifications')
+@ApiBearerAuth('JWT-auth')
 export class NotificationController {
   constructor(private notificationService: NotificationService) {}
 
@@ -14,12 +23,41 @@ export class NotificationController {
   @ApiResponse({
     status: 200,
     description: 'Get all notifications',
+    type: [NotificationEntity],
+  })
+  @ApiOperation({
+    summary: 'Get all notifications',
+    description: 'Get all notifications for the authenticated user',
+  })
+  @ApiBadRequestResponse({
+    status: 400,
+    description: 'Bad Data Provided',
+  })
+  @ApiInternalServerErrorResponse({
+    status: 500,
+    description: 'Internal Server Error',
   })
   async getAllNotifications(): Promise<Message> {
     return await this.notificationService.getAllNotifications();
   }
 
   @Put('/read/:id')
+  @ApiResponse({
+    status: 200,
+    description: 'Update notification status',
+  })
+  @ApiOperation({
+    summary: 'Update notification status',
+    description: 'Update notification status for the authenticated user',
+  })
+  @ApiBadRequestResponse({
+    status: 400,
+    description: 'Bad Data Provided',
+  })
+  @ApiInternalServerErrorResponse({
+    status: 500,
+    description: 'Internal Server Error',
+  })
   async updateNotificationStatus(
     @Param('id') notificationId: string,
   ): Promise<Message> {
