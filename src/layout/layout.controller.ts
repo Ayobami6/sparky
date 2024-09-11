b853446783c3d20a96894099b2955bb1485c6257
Ayobami Alaran A. <ayobamidele006@gmail.com>
2024-09-11 15:56:35 +0100

    feat: Enhance API documentation for layouts
    Add comprehensive Swagger documentation to the layout module. This includes detailed descriptions, examples, and response models for all layout-related endpoints, improving API discoverability and clarity for developers.

--- src/layout/layout.controller.ts
+++ src/layout/layout.controller.ts
@@ -3,15 +3,41 @@ import { LayoutService } from './layout.service';
 import { AdminAuthGuard } from 'src/auth/jwt-admin-authguard';
 import { CreateLayoutDto } from './dto/create-layout.dto';
 import { Message } from 'src/user/types';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiInternalServerErrorResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { LayoutEntity } from './layout.entity';
 
 @ApiTags('layouts')
 @Controller('layouts')
+@ApiBearerAuth('JWT-auth')
 export class LayoutController {
   constructor(private layoutService: LayoutService) {}
 
   @UseGuards(AdminAuthGuard)
   @Post('create')
+  @ApiResponse({
+    status: 201,
+    description: 'Create a new layout',
+    type: LayoutEntity,
+  })
+  @ApiOperation({
+    summary: 'Create a new layout',
+    description: 'Create a new layout',
+  })
+  @ApiBadRequestResponse({
+    status: 400,
+    description: 'Bad Data Provided',
+  })
+  @ApiInternalServerErrorResponse({
+    status: 500,
+    description: 'Internal Server Error',
+  })
   async createLayout(
     @Body() createLayoutDto: CreateLayoutDto,
   ): Promise<Message> {
@@ -19,12 +45,46 @@ export class LayoutController {
   }
 
   @UseGuards(AdminAuthGuard)
+  @ApiOperation({
+    summary: 'Get layout by type',
+    description: 'Get layout by type',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Get layout by type',
+    type: LayoutEntity,
+  })
+  @ApiBadRequestResponse({
+    status: 400,
+    description: 'Bad Data Provided',
+  })
+  @ApiInternalServerErrorResponse({
+    status: 500,
+    description: 'Internal Server Error',
+  })
   @Put('edit')
   async editLayout(@Body() createLayoutDto: CreateLayoutDto): Promise<Message> {
     return await this.layoutService.editLayout(createLayoutDto);
   }
 
   @Get()
+  @ApiBadRequestResponse({
+    status: 400,
+    description: 'Bad Data Provided',
+  })
+  @ApiInternalServerErrorResponse({
+    status: 500,
+    description: 'Internal Server Error',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Get layout by type',
+    type: LayoutEntity,
+  })
+  @ApiOperation({
+    summary: 'Get layout by type',
+    description: 'Get layout by type',
+  })
   async getLayoutByType(@Body() typeBody: any): Promise<Message> {
     const { type } = typeBody;
     const layout = await this.layoutService.findLayoutByType(type);
--- src/layout/layout.entity.ts
+++ src/layout/layout.entity.ts
@@ -8,6 +8,7 @@ import {
 } from 'typeorm';
 import { Banner, Category, FaqItem } from './layout-types';
 import { v4 as uuid } from 'uuid';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class LayoutEntity {
@@ -15,24 +16,68 @@ export class LayoutEntity {
   _id: ObjectId;
 
   @PrimaryGeneratedColumn('uuid')
+  @ApiProperty({
+    type: 'string',
+    format: 'uuid',
+    description: 'Unique identifier for the layout',
+  })
   id: string;
 
   @Column()
+  @ApiProperty({
+    type: 'string',
+    description: 'Type of layout',
+  })
   type: string;
 
   @Column()
+  @ApiProperty({
+    type: 'array',
+    items: {
+      type: 'object',
+      properties: { question: { type: 'string' }, answer: { type: 'string' } },
+    },
+    description: 'List of frequently asked questions and their answers',
+  })
   faq: FaqItem[];
 
   @Column()
+  @ApiProperty({
+    type: 'array',
+    items: { type: 'string' },
+    description: 'List of categories in the layout',
+  })
   categories: Category[];
 
   @Column()
+  @ApiProperty({
+    type: 'object',
+    properties: {
+      title: { type: 'string' },
+      image: {
+        type: 'object',
+        properties: { public_id: { type: 'string' }, url: { type: 'string' } },
+      },
+      subtitle: { type: 'string' },
+    },
+    description: 'Layout banner',
+  })
   banner: Banner;
 
   @Column()
+  @ApiProperty({
+    type: 'string',
+    format: 'date-time',
+    description: 'Timestamp for when the layout was created',
+  })
   createdAt: Date;
 
   @Column()
+  @ApiProperty({
+    type: 'string',
+    format: 'date-time',
+    description: 'Timestamp for when the layout was last updated',
+  })
   updatedAt: Date;
 
   @BeforeInsert()
